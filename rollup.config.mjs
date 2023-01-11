9f11e358d7b6a4793cbaacc54ee9de411a5160fc
artemshar <artem.ude@gmail.com>
2023-01-12 00:48:12 +0300

    Added Tests example, update configs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from '@rollup/plugin-terser';
-import postcss from "rollup-plugin-postcss";
+import postcss from "rollup-plugin-postcss"
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';;
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -23,11 +24,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
       postcss(),
+      terser(),
     ],
   },
   {
